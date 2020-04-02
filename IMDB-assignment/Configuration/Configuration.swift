dd962b884fd8c25bce9ca7d00b64f26cade32981
Mendy Edri <mendy.edri@carlsonwagonlit.com>
2020-04-02 19:15:52 +0300

    Configuration and ENV types created to control the app environment by changing the global Configuration variable

---
+++ IMDB-assignment/Configuration/Configuration.swift
@@ -0,0 +1,15 @@
+//
+//  Configuration.swift
+//  IMDB-assignment
+//
+//  Created by Mendy Edri on 02/04/2020.
+//  Copyright © 2020 CWT. All rights reserved.
+//
+
+import Foundation
+
+public final class Configuration {
+    private init() {}
+    
+    public static var env: ENV = .production
+}
---
+++ IMDB-assignment/Configuration/ENV.swift
@@ -0,0 +1,14 @@
+//
+//  ENV.swift
+//  IMDB-assignment
+//
+//  Created by Mendy Edri on 02/04/2020.
+//  Copyright © 2020 CWT. All rights reserved.
+//
+
+import Foundation
+
+public enum ENV {
+    case production
+    case stage
+}

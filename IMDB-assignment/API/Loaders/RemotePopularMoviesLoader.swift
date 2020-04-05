b4bdea32a388f7e513576aac3047b4463db89039
Mendy Edri <mendy.edri@carlsonwagonlit.com>
2020-04-05 10:16:21 +0300

    Created RemotePopularMoviesLoader

--- IMDB-assignment/API/Loaders/RemotePopularMoviesLoader.swift
+++ IMDB-assignment/API/Loaders/RemotePopularMoviesLoader.swift
@@ -10,9 +10,37 @@ import Foundation
 import lit_network
 
 struct RemotePopularMoviesLoader: RemoteLoader {
-    var httpClient: HTTPClient
+    public typealias Result = Swift.Result<PopularMoviesList, RemoteLoader.Error>
     
-    init(http: HTTPClient) {
+    public var httpClient: HTTPClient
+    
+    public init(http: HTTPClient) {
         self.httpClient = http
+    }
+    
+    public func load(with url: URL, completion: @escaping (Result) -> Void) {
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringCacheData, timeoutInterval: 30)
+        httpClient.get(with: request) { result in
+            switch result {
+            case let .success(data, response):
+                completion(PopularMoviesListDataMapper.map(data: data, from: response))
+            
+            case .failure:
+                completion(.failure(.invalidData))
+            }
+        }
+    }
+}
+
+struct PopularMoviesListDataMapper {
+    private init() {}
+    private static var ok200: Int { 200 }
+    
+    static func map(data: Data, from response: HTTPURLResponse) -> RemotePopularMoviesLoader.Result {
+        guard response.statusCode == ok200,
+            let movieList = try? JSONDecoder().decode(PopularMoviesList.self, from: data) else {
+                return .failure(.invalidData)
+        }
+        return .success(movieList)
     }
 }

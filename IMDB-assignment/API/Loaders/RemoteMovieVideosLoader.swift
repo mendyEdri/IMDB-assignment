74cbc6841bf677a6939472613e9bcf4a5e68616f
Mendy Edri <mendy.edri@carlsonwagonlit.com>
2020-04-05 10:15:51 +0300

    Created RemoteMovieVideosLoader t

--- IMDB-assignment/API/Loaders/RemoteMovieVideosLoader.swift
+++ IMDB-assignment/API/Loaders/RemoteMovieVideosLoader.swift
@@ -10,9 +10,36 @@ import Foundation
 import lit_network
 
 struct RemoteMovieVideosLoader: RemoteLoader {
+    typealias Result = Swift.Result<[MovieVideos], RemoteLoader.Error>
+    
     var httpClient: HTTPClient
     
     init(http: HTTPClient) {
         self.httpClient = http
+    }
+    
+    func load(from URL: URL, completion: @escaping (Result) -> Void) {
+        let request = URLRequest(url: URL, cachePolicy: .useProtocolCachePolicy, timeoutInterval: 30)
+        httpClient.get(with: request) { result in
+            switch result {
+            case let .success(data, response):
+                completion(RemoteMovieVideosDataMapper.map(data: data, from: response))
+            case .failure:
+                completion(.failure(.serverError))
+            }
+        }
+    }
+}
+
+fileprivate struct RemoteMovieVideosDataMapper {
+    private init() {}
+    private static var ok200: Int { 200 }
+    
+    static func map(data: Data, from response: HTTPURLResponse) -> RemoteMovieVideosLoader.Result {
+        guard response.statusCode == ok200,
+        let videoList = try? JSONDecoder().decode(MovieVideosList.self, from: data) else {
+            return .failure(.invalidData)
+        }
+        return .success(videoList.results)
     }
 }
